5ab225874e3d4baa55ac1fa35bd071cf4a0652bd
Daria Kozielecka <38034382+dkozielecka@users.noreply.github.com>
2019-10-24 19:55:36 +0200

    Update pokedex-routing.module.ts

--- src/app/pokedex/pokedex-routing.module.ts
+++ src/app/pokedex/pokedex-routing.module.ts
@@ -15,7 +15,7 @@ const routes: Routes = [
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot( routes )
+    RouterModule.forChild( routes )
   ]
 })
 export class PokedexRoutingModule { }
